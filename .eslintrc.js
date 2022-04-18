8023175be50c94b1a03948a5d1fec972991f01b6
minji <eunoia2040@gmail.com>
2022-04-18 15:22:53 +0900

    feat: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,55 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
-    node: true,
     jest: true,
+    node: true,
   },
   extends: [
-    '@react-native-community',
-    'eslint:recommended',
-    'eslint-config-prettier',
-    'plugin:react/recommended',
-    'plugin:import/recommended',
+    "@react-native-community",
+    "eslint:recommended",
+    "eslint-config-prettier",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: 12,
+    sourceType: "module",
   },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
+    "import/ignore": ["react-native"],
   },
-  plugins: ['react', 'prettier'],
+  plugins: ["react", "react-hooks", "prettier"],
   rules: {
-    'prettier/prettier': 'error',
-    'react/prop-types': 'off',
-    'import/order': [
-      'error',
+    "prettier/prettier": "error",
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-var": "error",
+    "no-unused-vars": "warn",
+    "func-style": ["error", "expression"],
+    "react/prop-types": "warn",
+    "import/order": [
+      "error",
       {
-        groups: ['builtin', 'external', 'internal', ['parent', 'sibling']],
+        groups: ["builtin", "external", "internal", ["parent", "sibling"]],
         pathGroups: [
           {
-            pattern: 'react',
-            group: 'external',
-            position: 'before',
+            pattern: "react",
+            group: "external",
+            position: "before",
           },
         ],
-        pathGroupsExcludedImportTypes: ['react'],
-        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ["react"],
+        "newlines-between": "always",
         alphabetize: {
-          order: 'asc',
+          order: "asc",
           caseInsensitive: true,
         },
       },
     ],
-    'import/no-named-as-default': 0,
   },
 };
