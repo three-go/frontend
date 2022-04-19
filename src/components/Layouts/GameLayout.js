b7ae4cce938bda34599603b2148420cad36d10f3
minji <eunoia2040@gmail.com>
2022-04-19 20:45:35 +0900

    feat(GameLayout): Add GameLayout component

---
+++ src/components/Layouts/GameLayout.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { View, StyleSheet, SafeAreaView } from "react-native";
+
+import { GameHeader } from "../index";
+
+/*
+  [GameLayout 호출 방식 예시]
+  <GameLayout>
+    <View>게임 보드 내용</View>
+  </GameLayout>
+*/
+const GameLayout = ({ children }) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <GameHeader onPressBack={() => console.log("go back to home!")} />
+      </View>
+      <View style={styles.gameBoard}>{children}</View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+  header: {
+    height: "10%",
+    backgroundColor: "pink",
+  },
+  gameBoard: {
+    height: "90%",
+    backgroundColor: "lightblue",
+  },
+});
+
+export default GameLayout;
