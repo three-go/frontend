a1f3b43b30da271904ce13abf42f7bff83367001
woojeong kong <coolstevevc@gmail.com>
2022-04-24 02:16:20 +0900

    feat: Add handling when camera is not authorized and Change css style

--- src/features/game1/presenter/FaceRecognition.js
+++ src/features/game1/presenter/FaceRecognition.js
@@ -1,19 +1,30 @@
 import React from "react";
 
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Linking } from "react-native";
 import { RNCamera } from "react-native-camera";
+import RNExitApp from "react-native-exit-app";
 
-const FaceRecognition = ({ selectedDirection, handlerFacePosition }) => {
+import { SmallButton } from "../../../components";
+
+const FaceRecognition = ({
+  selectedDirection,
+  handlerFacePosition,
+  cameraPermissionStatus,
+  setCameraPermissionStatus,
+  isDirectionPreviewTextShow,
+}) => {
+  const openSettingOption = async () => {
+    await Linking.openSettings();
+    RNExitApp.exitApp();
+  };
+
   return (
     <View style={styles.container}>
       <RNCamera
         style={styles.preview}
         type={RNCamera.Constants.Type.front}
-        androidCameraPermissionOptions={{
-          title: "Permission to use camera",
-          message: "We need your permission to use your camera",
-          buttonPositive: "Ok",
-          buttonNegative: "Cancel",
+        onStatusChange={({ cameraStatus }) => {
+          setCameraPermissionStatus(cameraStatus);
         }}
         captureAudio={false}
         onFacesDetected={handlerFacePosition}
@@ -22,12 +33,33 @@ const FaceRecognition = ({ selectedDirection, handlerFacePosition }) => {
         }
         faceDetectionLandmarks={RNCamera.Constants.FaceDetection.Landmarks.all}
         faceDetectionMode={RNCamera.Constants.FaceDetection.Mode.accurate}
+        notAuthorizedView={<View />}
       />
-      <View style={styles.previewDirection}>
-        <Text style={styles.text}>
-          {selectedDirection.direction?.toUpperCase()}
-        </Text>
-      </View>
+      {cameraPermissionStatus === "NOT_AUTHORIZED" && (
+        <View style={styles.notAuthorizedViewContainer}>
+          <Text style={styles.notAuthorizedViewTitle}>
+            권한 승인이 필요 합니다.{"\n"}
+          </Text>
+          <Text>
+            해당 서비스를 사용하기 위해서는 카메라 권한 승인이 필요합니다. 해당
+            권한을 승인하지 않으면 서비스 이용이 제한됩니다.{"\n"}
+          </Text>
+          <SmallButton
+            content="설정으로 가기"
+            color="#c92a2a"
+            onPress={() => {
+              openSettingOption();
+            }}
+          />
+        </View>
+      )}
+      {isDirectionPreviewTextShow && selectedDirection.direction && (
+        <View style={styles.previewDirection}>
+          <Text style={styles.directionText}>
+            {selectedDirection.direction?.toUpperCase()}
+          </Text>
+        </View>
+      )}
     </View>
   );
 };
@@ -51,14 +83,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: "100%",
     height: "100%",
-    borderBottomWidth: 1,
-    borderTopWidth: 1,
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
-    borderColor: "red",
+    borderBottomWidth: 2,
+    borderTopWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderColor: "#00E0EA",
   },
-  text: {
+  directionText: {
     fontSize: 100,
+  },
+  notAuthorizedViewContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "80%",
+    height: "50%",
+    padding: "5%",
+  },
+  notAuthorizedViewTitle: {
+    fontSize: 20,
+    fontWeight: "900",
   },
 });
 
