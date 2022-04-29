d1ba79769ea06123cff4b55bdbb6c7a2e2e7eb58
minji <eunoia2040@gmail.com>
2022-04-29 15:32:53 +0900

    feat(MapCell): Add MapCell component

--- src/features/game1/presenter/index.js
+++ src/features/game1/presenter/index.js
@@ -3,5 +3,13 @@ import Game1 from "../presenter/Game1";
 import Map from "../presenter/Map";
 import DefaultMap from "./DefaultMap";
 import FaceDirectionRecord from "./FaceDirectionRecord";
+import MapCell from "./MapCell";
 
-export { DefaultMap, FaceDirectionRecord, FaceRecognition, Game1, Map };
+export {
+  DefaultMap,
+  FaceDirectionRecord,
+  FaceRecognition,
+  Game1,
+  Map,
+  MapCell,
+};
