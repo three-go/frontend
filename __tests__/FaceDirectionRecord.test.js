2bef3c25a307abfffe1a41afd3459eb2b3a2d6a8
minji <eunoia2040@gmail.com>
2022-05-22 02:12:34 +0900

    test: Add FaceDirectionRecord

---
+++ __tests__/FaceDirectionRecord.test.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react-native";
+import "@testing-library/react-native";
+import FaceDirectionRecord from "../src/features/faceGo/presenter/FaceDirectionRecord";
