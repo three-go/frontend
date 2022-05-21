28106786edd0de09ce9596390c57ccf006ef870f
MINJI <54696956+CoRoRo10@users.noreply.github.com>
2022-05-22 02:18:58 +0900

    Delete FaceDirectionRecord.test.js

--- __tests__/FaceDirectionRecord.test.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { render, fireEvent } from "@testing-library/react-native";
-import "@testing-library/react-native";
-import FaceDirectionRecord from "../src/features/faceGo/presenter/FaceDirectionRecord";
